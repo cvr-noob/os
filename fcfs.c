170f0656d940dea2ddb94a20bb3c93891a364cac
cvr-noob <srichaitanyavictims2023@gmail.com>
2025-04-22 20:40:31 +0000

    FCFS

---
+++ fcfs.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+typedef struct Process
+{
+    int id, at, bt, ct, wt, tat;
+} process;
+
+void calcTimes(process p[], int n);
+
+int main()
+{
+    int n;
+    printf("Enter no. of processes: ");
+    scanf("%d", &n);
+
+    process p[n];
+    for (int i = 0; i < n; i++)
+    {
+        p[i].id = i + 1;
+        printf("\nEnter arrival time for p[%d]: ", p[i].id);
+        scanf("%d", &p[i].at);
+        printf("\nEnter burst time for p[%d]: ", p[i].id);
+        scanf("%d", &p[i].bt);
+    }
+
+    calcTimes(p, n);
+
+    printf("\n%-5s | %-12s | %-10s | %-15s | %-12s | %-16s\n",
+           "PID", "Arrival Time", "Burst Time", "Completion Time", "Waiting Time", "Turnaround Time");
+    printf("---------------------------------------------------------------------------------------------\n");
+
+    for (int i = 0; i < n; i++)
+        printf("%-5d | %-12d | %-10d | %-15d | %-12d | %-16d\n",
+               p[i].id, p[i].at, p[i].bt, p[i].ct, p[i].wt, p[i].tat);
+
+    return 0;
+}
+
+void calcTimes(process p[], int n)
+{
+    p[0].ct = p[0].at + p[0].bt;
+    int total_wt = p[0].wt = p[0].at;
+    int total_tat = p[0].tat = p[0].bt;
+
+    for (int i = 0; i < n; i++)
+    {
+        p[i].wt = p[i - 1].ct + p[i].at;
+        p[i].ct = p[i].at + p[i].wt + p[i].bt;
+        p[i].tat = p[i].ct - p[i].at;
+
+        total_tat += p[i].tat;
+        total_wt += p[i].wt;
+    }
+
+    printf("\nAverage waiting time: %.2f", (float)total_wt / 2);
+    printf("\nAverage turnaround time: %.2f", (float)total_tat / 2);
+}
