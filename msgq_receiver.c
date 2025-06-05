cc3a0b08b4c189d8d816d2795484c39916fe2381
CVR Noob <srichaitanyavictims2023@gmail.com>
2025-06-05 22:02:51 +0530

    Update msgq_receiver.c

--- msgq_receiver.c
+++ msgq_receiver.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
 typedef struct message
 {
     long type;
-    char text[256];
+    char text[128];
 } message;
 
 int main()
 {
-    key_t key = ftok("./msgq", 65);
+    // Generate Key
+    int key = ftok("./msgq", 65);
+    
+    // Get message queue
     int msgid = msgget(key, 0);
 
+    // Receive message
     message msg;
     msgrcv(msgid, &msg, sizeof(msg.text), 1, 0);
 
-    printf("msgq has: %s\n", msg.text);
-
-    return 0;
+    // Print
+    printf("Received: %s\n", msg.text);
 }
