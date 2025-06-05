225c56e7f0775f0054e0a8985127a53c3a6a76ce
CVR Noob <srichaitanyavictims2023@gmail.com>
2025-06-05 21:39:03 +0530

    Update shm.c

--- shm.c
+++ shm.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/types.h>
 
-#define SHM_SIZE 1024
+#define SHM_SIZE 128
 
 int main()
 {
-    key_t key = ftok("shmfile", 65);
-    int shmid = shmget(key, SHM_SIZE, 0666 | IPC_CREAT);
+    // Generate key
+    int key = ftok("shmfile", 65);
+
+    // Get Shared Memory
+    int shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT);
+
+    // Attach Shared Memory
     char *data = shmat(shmid, NULL, 0);
 
-    printf("Enter data: ");
-    scanf(" %[^\n]", data);
-    printf("Data written into shared memory.\n");
+    // User Input
+    printf("Enter data into the shared memory: ");
+    scanf("%[^\n]", data);
 
-    printf("Data in the shared memory: %s\n", data);
+    // Print
+    printf("Data inside shared memory: ", data);
 
+    // Cleanup
     shmdt(data);
     shmctl(shmid, IPC_RMID, NULL);
 }
